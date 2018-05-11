f78ab122c05b512823d9cafc9902b2b10eb0a6f3
Deepak Singh Kushwah <deepaksinghkushwah@gmail.com>
2018-05-11 12:41:02 +0530

    nethttp started

--- test1/template3/main.go
+++ test1/template3/main.go
@@ -2,16 +2,21 @@ package main
 
 import (
 	"html/template"
+	"log"
 	"os"
 	"time"
+
+	"github.com/leekchan/accounting"
 )
 
-func customFormat(t time.Time) string {
-	return t.Format("02-01-2006")
+func customFormat(t string) string {
+	newT, _ := time.Parse("01-02-2011", t)
+	return newT.Format("02-01-2006")
 }
 
-func currency(s string) string {
-	return "$" + s
+func currency(s float64) string {
+	ac := accounting.Accounting{Symbol: "$", Precision: 2}
+	return ac.FormatMoney(s)
 }
 
 var fm = template.FuncMap{
@@ -20,14 +25,16 @@ var fm = template.FuncMap{
 }
 
 type myData struct {
-	date   time.Time
-	amount float64
+	Date   string
+	Amount float64
 }
 
 func main() {
-	t := time.Now()
-	data := myData{date: t, amount: 5000.00}
+	data := myData{Date: "08-05-2018", Amount: 5000.55}
 
 	tpl := template.Must(template.New("").Funcs(fm).ParseFiles("time.html"))
-	tpl.ExecuteTemplate(os.Stdout, "time.html", data)
+	err := tpl.ExecuteTemplate(os.Stdout, "time.html", data)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
