310048d2aecce430979c150afbe93818fe210fad
Deepak Singh Kushwah <deepaksinghkushwah@gmail.com>
2019-02-19 04:39:51 +0530

    sql test

--- sqlite-test1/main.go
+++ sqlite-test1/main.go
@@ -10,20 +10,23 @@ import (
 )
 
 func main() {
-	db, _ := sql.Open("sqlite3", "./database.sqlite3")
-
+	db, err := sql.Open("sqlite3", "./database.sqlite3")
+	checkError(err)
 	var id int
 	var username, password, email string
 
 	// insert into table
-	stmt, _ := db.Prepare("INSERT INTO user (username, email, password) values (?, ?, ?)")
-	for i := 0; i < 5; i++ {
-		stmt.Exec("sam"+strconv.Itoa(i), "sam"+strconv.Itoa(i)+"@localhost.com", "123456")
+	stmt, err := db.Prepare("INSERT INTO user (firstname,lastname,username, email, password) values (?, ?, ?, ?, ?)")
+	checkError(err)
+	for i := 20; i <= 30; i++ {
+		_, err := stmt.Exec("sam", strconv.Itoa(i), "sam"+strconv.Itoa(i), "sam"+strconv.Itoa(i)+"@localhost.com", "123456")
+		checkError(err)
 	}
 
 	defer stmt.Close()
 
-	rows, _ := db.Query("SELECT id, username, password, email FROM user WHERE id > ?", 0)
+	rows, err := db.Query("SELECT id, username, password, email FROM user WHERE id > ?", 0)
+	checkError(err)
 	defer rows.Close()
 	// fetching multiple rows
 	for rows.Next() {
@@ -36,11 +39,18 @@ func main() {
 	}
 
 	// fetching single row and display its content
-	stmt2, _ := db.Prepare("SELECT id, username, password, email FROM user where id = ?")
+	stmt2, err := db.Prepare("SELECT id, username, password, email FROM user where id = ?")
+	checkError(err)
 	stmt2.QueryRow("MAX(id)").Scan(&id, &username, &password, &email)
 	defer stmt2.Close()
 	fmt.Println("--------------------------------------")
 	fmt.Println("Fetching single row where last id is", id)
 	fmt.Println("--------------------------------------")
 	fmt.Println(id, username, password, email)
+}
+
+func checkError(e error) {
+	if e != nil {
+		log.Println(e)
+	}
 }
