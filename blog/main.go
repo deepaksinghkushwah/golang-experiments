bd3d780ac1215dc5adcd8a30c9acb06bdefc31fc
Deepak Singh Kushwah <deepaksinghkushwah@gmail.com>
2019-03-12 23:14:45 +0530

    updates

--- blog/main.go
+++ blog/main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"encoding/gob"
-	"fmt"
 	"log"
 	"net/http"
 	"text/template"
 
-	"github.com/gorilla/securecookie"
 	"github.com/gorilla/sessions"
 
 	"github.com/gorilla/mux"
@@ -16,44 +13,22 @@ import (
 var t *template.Template
 var store *sessions.CookieStore
 
-type User struct {
-	Username      string
-	Authenticated bool
-}
-
-func getUser(s *sessions.Session) User {
-	val := s.Values["user"]
-	var user = User{}
-	user, ok := val.(User)
-	if !ok {
-		return User{Authenticated: false}
-	}
-	return user
-}
-
 func init() {
-	authKeyOne := securecookie.GenerateRandomKey(64)
-	encryptionKeyOne := securecookie.GenerateRandomKey(32)
 	t = template.Must(template.ParseGlob("templates/*.html"))
-	store = sessions.NewCookieStore(authKeyOne, encryptionKeyOne)
-	store.Options = &sessions.Options{
-		MaxAge:   60 * 15,
-		HttpOnly: true,
-	}
-	gob.Register(User{})
+	store = sessions.NewCookieStore([]byte("t0p-s3cr3t"))
+	store.Options.HttpOnly = true
 }
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/hello", helloHandler).Methods("GET")
-	r.HandleFunc("/goodbye", goodbyeHandler).Methods("GET")
+	r.HandleFunc("/forbidden", forbiddenHandler).Methods("GET")
 
 	r.HandleFunc("/login", loginGetHandler).Methods("GET")
 	r.HandleFunc("/login", loginPostHandler).Methods("POST")
 
 	r.HandleFunc("/test", testHandler).Methods("GET")
 	r.HandleFunc("/logout", logoutHandler)
-	r.HandleFunc("/forbidden", forbiddenHandler)
 
 	r.HandleFunc("/", handler).Methods("GET")
 
@@ -62,15 +37,6 @@ func main() {
 
 	http.Handle("/", r)
 	http.ListenAndServe(":8080", nil)
-}
-
-func forbiddenHandler(w http.ResponseWriter, r *http.Request) {
-	page := struct {
-		PageTitle string
-	}{
-		PageTitle: "403 Forbidden",
-	}
-	t.ExecuteTemplate(w, "forbidden.html", page)
 }
 
 func loginGetHandler(w http.ResponseWriter, r *http.Request) {
@@ -86,14 +52,7 @@ func loginPostHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	username := r.PostForm.Get("username")
 	session, _ := store.Get(r, "session")
-
-	user := &User{
-		Username:      username,
-		Authenticated: true,
-	}
-
-	session.Values["user"] = user
-
+	session.Values["username"] = username
 	session.Save(r, w)
 	http.Redirect(w, r, "/test", 301)
 }
@@ -122,63 +81,62 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	t.ExecuteTemplate(w, "index.html", page)
 }
 
-func goodbyeHandler(w http.ResponseWriter, r *http.Request) {
+func forbiddenHandler(w http.ResponseWriter, r *http.Request) {
 	page := struct {
 		PageTitle string
 		Name      string
-		Age       int
 	}{
-		PageTitle: "Good Bye Handler",
-		Name:      "Good Bye",
-		Age:       35,
+		PageTitle: "403",
+		Name:      "403",
 	}
-	t.ExecuteTemplate(w, "index.html", page)
+	t.ExecuteTemplate(w, "forbidden.html", page)
 }
 
 func testHandler(w http.ResponseWriter, r *http.Request) {
-	session, err := store.Get(r, "session")
-	checkErr(err)
-
-	user := getUser(session)
-	fmt.Println(user)
-	if auth := user.Authenticated; !auth {
+	session, _ := store.Get(r, "session")
+	untyped, ok := session.Values["username"]
+	if !ok {
 		http.Redirect(w, r, "/forbidden", 301)
+		return
+	}
+	username, ok := untyped.(string)
+	if !ok || username == "" {
+		http.Redirect(w, r, "/forbidden", 301)
+		return
 	}
 	page := struct {
 		PageTitle string
 		Name      string
 	}{
-		PageTitle: "Good Bye Handler",
-		Name:      "Loggedin",
+		PageTitle: "TEST Handler",
+		Name:      username,
 	}
 	t.ExecuteTemplate(w, "test.html", page)
 }
 
 func loginCheck(r *http.Request) bool {
 	session, _ := store.Get(r, "session")
-	if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
+	untyped, ok := session.Values["username"]
+	if !ok {
 		return false
-	} else {
-		fmt.Println(auth, ok, session.Values["username"].(string))
-		return true
 	}
+	_, ok = untyped.(string)
+	if !ok {
+		return false
+	}
+	return true
 }
 func logoutHandler(w http.ResponseWriter, r *http.Request) {
 	session, err := store.Get(r, "session")
 	checkErr(err)
-	session.Values["user"] = User{}
-	session.Options.MaxAge = -1
-
+	session.Values["username"] = ""
 	err = session.Save(r, w)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	http.Redirect(w, r, "/test", 301)
+	checkErr(err)
+	http.Redirect(w, r, "/", 301)
 }
 
 func checkErr(e error) {
 	if e != nil {
-		log.Println(e)
+		log.Panic(e)
 	}
 }
